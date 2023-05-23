07969596eacc1f320126657f12c507781419a7e7
kondury <65617656+kondury@users.noreply.github.com>
2023-05-23 18:05:12 +0300

    Update MongockConfig.java

--- library-web/library-webflux-mongo/src/main/java/com/github/kondury/library/config/MongockConfig.java
+++ library-web/library-webflux-mongo/src/main/java/com/github/kondury/library/config/MongockConfig.java
@@ -1,23 +1,15 @@
 package com.github.kondury.library.config;
 
 import com.github.kondury.library.migration.LibraryInitializationChangeUnit;
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
 import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
 import io.mongock.driver.mongodb.reactive.driver.MongoReactiveDriver;
 import io.mongock.runner.springboot.MongockSpringboot;
 import io.mongock.runner.springboot.base.MongockInitializingBeanRunner;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 @ConditionalOnProperty(name = "mongock.enabled")
 @EnableConfigurationProperties(MongoDbProperties.class)
