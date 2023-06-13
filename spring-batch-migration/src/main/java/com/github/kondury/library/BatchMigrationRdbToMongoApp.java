5931aab472957bf34282afc9c22a30fd30c274bd
kondury <65617656+kondury@users.noreply.github.com>
2023-06-13 13:56:20 +0300

    hw14 Spring Batch Migration RDB -> MongoDB - initial commit

---
+++ spring-batch-migration/src/main/java/com/github/kondury/library/BatchMigrationRdbToMongoApp.java
@@ -0,0 +1,12 @@
+package com.github.kondury.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchMigrationRdbToMongoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchMigrationRdbToMongoApp.class);
+    }
+}
