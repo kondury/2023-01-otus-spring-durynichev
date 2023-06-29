6d5822e7b4bd71745441ae1e931bf1bfb2b802d9
kondury <65617656+kondury@users.noreply.github.com>
2023-06-29 15:00:17 +0300

    hw16 Add Spring Actuator - extracted actuator security into separate config

--- library-actuator/src/main/java/com/github/kondury/library/actuator/ActuatorSecurityConfig.java
+++ library-actuator/src/main/java/com/github/kondury/library/actuator/ActuatorSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration/*(proxyBeanMethods = false)*/
+@Configuration
 public class ActuatorSecurityConfig {
 
     @Bean
